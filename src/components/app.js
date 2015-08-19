c0aaf4be5205ba5849f914e14924b864496c289b
Leonardo Couto <leonardowrc@gmail.com>
2015-08-19 20:49:04 -0300

    Add lovely message.

--- src/components/app.js
+++ src/components/app.js
@@ -19,18 +19,25 @@ export default class App extends React.Component {
   }
 
   render() {
+    let content;
     let style = {
       marginLeft: '35px',
       marginBottom: '30px'
     }
 
+    if (this.state.cat) {
+      content = <img src={this.state.cat} />;
+    } else {
+      content = "Waiting in the Instagram api queue ... You catta be kitten me, purr.";
+    }
+
     return(
       <div>
         <center>
-          <div style={style} >
+          <div style={style}>
             <img src='/assets/cat.png' />
           </div>
-          <img src={this.state.cat} />
+          {content} 
         </center>
       </div>
     )
